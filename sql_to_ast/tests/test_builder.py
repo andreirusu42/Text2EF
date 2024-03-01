3d60b42379c11b35f2d2d3dc667263c67b6571e2
Rusu Andrei-Cristian <hello@andreirusu.dev>
2024-03-01 23:48:09 +0200

    more restructuring, create env for testing

---
+++ sql_to_ast/tests/test_builder.py
@@ -0,0 +1,12 @@
+import unittest
+
+from src.builder import Sql2AstBuilder
+
+
+class BuilderTest(unittest.TestCase):
+    def test_build(self):
+        self.assertEqual(1, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
