26bcaf1657144ce31e1fc739b42a146ddeda7cbe
Rusu Andrei-Cristian <hello@andreirusu.dev>
2024-03-06 18:38:25 +0200

    add logic for group by clause

--- entity-framework/Activity1.cs
+++ entity-framework/Activity1.cs
@@ -5,15 +5,21 @@ class Activity1Test
 {
     public static void Test()
     {
-        using var context = new allergy_1Context();
+        using var context = new Allergy1Context();
 
         // var sql = "SELECT count(*) FROM Has_allergy AS T1 JOIN Allergy_type AS T2 ON T1.allergy  =  T2.allergy WHERE T2.allergytype  =  'food'";
 
         // var resultSql = context.Has_Allergy.FromSqlRaw(sql)
         // .FirstOrDefault();
 
-        var resultLinq = context.Has_Allergy.Join(context.Allergy_Type, T1 => T1.Allergy, T2 => T2.Allergy, (T1, T2) => new { T1, T2 })
-    .Where(row => row.T2.AllergyType == "food")
+        var resultLinq = context.HasAllergies.Join(context.AllergyTypes, T1 => T1.Allergy, T2 => T2.Allergy, (T1, T2) => new { T1, T2 })
+    .Where(row => row.T2.AllergyType1 == "food")
+    .GroupBy(row => new { row.T1.Allergy, row.T2.AllergyType1 })
+    .Select(group => new
+    {
+        Count = group.Count(),
+        x = group.Key.AllergyType1
+    })
     .Count();
 
         Console.WriteLine(resultLinq);
